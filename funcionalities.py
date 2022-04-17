7bf909024661dacf95de6be8d8e6297e2b186219
Paula Gómez Matos <paulagomezmatos@gmail.com>
2022-04-17 11:00:45 +0200

    Forgot to upload this one haha

--- funcionalities.py
+++ funcionalities.py
@@ -6,4 +6,4 @@ Provides some repetitive functionalities
 def format_list(text: str, separator: str) -> list:
     """Takes a comma separated string of items and returns
     a list with those"""
-    return list(map(lambda x: x.strip(), text.split(separator)))
+    return list(map(lambda x: x.strip().replace("'", ""), text.split(separator)))
